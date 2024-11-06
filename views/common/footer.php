fd978831fd81c2bb21d7bdfeed0f941dee9c3d0b
paythroughripam <ripum.k@paythrough.in>
2024-11-06 10:52:52 +0530

    common js change

--- views/common/footer.php
+++ views/common/footer.php
@@ -36,7 +36,7 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<script src="<?= static_url('js/custom.js') ?>"></script>
+<script src="<?= static_url('js/customn.js') ?>"></script>
 
 
     </html>
